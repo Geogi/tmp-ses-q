f010cbe0a59c0ad14fa904251a67c267455a511d
Paul Blouët <paulmoa@gmail.com>
2013-08-05 08:42:46 -0400

    commit before big move

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,15 @@ var bobP = Q.defer();
 
 // Actors
 var bank = new Bank();
-var alice = new Alice(bankP, bobP);
-var bob = new Bob(bankP);
+var alice = new Alice(bankP.promise, bobP.promise);
+var bob = new Bob(bankP.promise);
+console.log("Actors bank, alice and bob created.");
 
 // Fulfill
 bankP.resolve(bank);
 bobP.resolve(bob);
+console.log("Promises fullfiled");
 
 // Act
 alice.buy("book", 10);
+console.log("Action complete.");
